87c04091218338d5247556430671c8d30bf72dda
Lecogoni <giraud.nicolas@me.com>
2022-05-25 17:53:54 +0000

    view show one product and method to make it work

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -11,13 +11,32 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductController extends AbstractController
 {
 
-    #[Route('/product', name: 'app_product')]
+    #[Route('/products', name: 'app_products')]
     public function index(ManagerRegistry $doctrine): Response
     {
         $products = $doctrine->getRepository(Product::class)->findAll();
 
         return $this->render('product/index.html.twig', [
             'products' => $products
+        ]);
+    }
+
+    /**
+     * show one product find by its slug get from url param slug
+     */
+    #[Route('/product/{slug}', name: 'app_product')]
+    public function show(ManagerRegistry $doctrine, $slug): Response
+    {
+        // query find one product by its slug
+        $product = $doctrine->getRepository(Product::class)->findOneBy(['slug' =>  $slug]);
+        
+        // if products isn't find send to page with all products
+        if (!$product) {
+            return $this->redirectToRoute('app_products');
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product
         ]);
     }
 }
