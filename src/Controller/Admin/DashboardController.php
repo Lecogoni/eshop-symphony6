1c2e77164466906842da4fec280a588acc8a8d6c
Lecogoni <giraud.nicolas@me.com>
2022-06-07 16:33:06 +0000

    link easy admin to Carier entity and it to admin dashboard menu

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Carrier;
 use App\Entity\Category;
 use App\Entity\Product;
 use App\Entity\User;
@@ -49,6 +50,7 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('User', 'fas fa-user', User::class);
         yield MenuItem::linkToCrud('Category', 'fa-solid fa-tag', Category::class);
         yield MenuItem::linkToCrud('Product', 'fa-solid fa-shirt', Product::class);
+        yield MenuItem::linkToCrud('Transporteur', 'fa-solid fa-truck-fast', Carrier::class);
         yield MenuItem::linkToLogout('Logout', 'fa-solid fa-right-from-bracket');
         //yield MenuItem::linkToRoute('app_register', 'fa fa-exit');
     }
